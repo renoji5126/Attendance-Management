b81c04c14e9e3cf7288649e818c46808ef5cccd4
renoji5126 <renoji5126@gmail.com>
2015-01-26 11:57:49 +0900

    change

--- public/javascripts/kintai.js
+++ public/javascripts/kintai.js
@@ -1,5 +1,7 @@
 $(function(){
   $(".kintai_days").children("input").change(function(){
     console.log(this);
+    $dom=$(this).parent();
+    console.log($dom.children(".kintai_day").text());
   });
 });
