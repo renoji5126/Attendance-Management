5ce3b6200a7676b93fec937f7630c2ca767b94ab
renoji5126 <renoji5126@gmail.com>
2015-01-28 00:03:47 +0900

    デザイン修正

--- public/javascripts/kintai.js
+++ public/javascripts/kintai.js
@@ -1,7 +1,14 @@
+
 $(function(){
   $(".kintai_days").children("input").change(function(){
     console.log(this);
     $dom=$(this).parent();
-    console.log($dom.children(".kintai_day").text());
+    var day = $dom.children(".kintai_day").text();
+    var month = $dom.children(".kintai_month").text();
+    var year = $dom.children(".kintai_year").text();
+    var start_time = $dom.children(".kintai_start_time").val();
+    var end_time = $dom.children(".kintai_end_time").val();
+    var regist_type = $dom.children(".kintai_type").val();
+    console.log(day,start_time,end_time,regist_type);
   });
 });
