aa887e14c114d8ba26bd658577ba54fb49b21ccc
renoji5126 <renoji5126@gmail.com>
2015-01-28 21:30:17 +0900

    値取得

--- public/javascripts/kintai.js
+++ public/javascripts/kintai.js
@@ -1,14 +1,14 @@
 
 $(function(){
-  $(".kintai_days").children("input").change(function(){
+  $("input").change(function(){
     console.log(this);
-    $dom=$(this).parent();
+    $dom=$(this).parent().parent();
     var day = $dom.children(".kintai_day").text();
-    var month = $dom.children(".kintai_month").text();
-    var year = $dom.children(".kintai_year").text();
-    var start_time = $dom.children(".kintai_start_time").val();
-    var end_time = $dom.children(".kintai_end_time").val();
-    var regist_type = $dom.children(".kintai_type").val();
+    var month = $(".kintai_month").text();
+    var year = $(".kintai_year").text();
+    var start_time = $dom.children("td").children(".kintai_start_time").val();
+    var end_time = $dom.children("td").children(".kintai_end_time").val();
+    var regist_type = $dom.children("td").children(".kintai_type").val();
     console.log(day,start_time,end_time,regist_type);
   });
 });
