94c937d5f93f8f3593bafda0a56fbe0e9ef802b3
renoji5126 <renoji5126@gmail.com>
2015-01-29 12:44:59 +0900

    fix

--- public/javascripts/kintai.js
+++ public/javascripts/kintai.js
@@ -10,10 +10,10 @@ $(function(){
     var end_time = $dom.children("td").children(".kintai_end_time").val();
     var regist_type = $dom.children("td").children(".kintai_type").val();
     console.log(year,month,day,start_time,end_time,regist_type);
-
+    // TODO month,dayを0パディングしないとだめぽよ
     $.ajax({
       type: "POST",
-      url: "/kintai/" + year + "/" + month + "/" + day + "/",
+      url: "/kintai/" + year + "/" + ('0' + month).slice(-2) + "/" + ('0' + day).slice(-2) + "/",
       data: "startTime=" + start_time + "&stopTime=" + end_time + "&type=" + regist_type 
     });
   });
