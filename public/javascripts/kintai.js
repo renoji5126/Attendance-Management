4cfb553db0d8716210075d817f8f168abf2ee88f
renoji5126 <renoji5126@gmail.com>
2015-02-02 18:58:26 +0900

    fix

--- public/javascripts/kintai.js
+++ public/javascripts/kintai.js
@@ -1,21 +1,23 @@
 
 $(function(){
-  $("input").change(function(){
-    console.log(this);
+  var regist = function(className){
     $dom=$(this).parent().parent();
     var day = $dom.children(".kintai_day").text();
     var month = $(".kintai_month").text();
     var year = $(".kintai_year").text();
     var start_time = $dom.children("td").children(".kintai_start_time").val();
-    var end_time = $dom.children("td").children(".kintai_end_time").val();
+    var stop_time = $dom.children("td").children(".kintai_stop_time").val();
     var regist_type = $dom.children("td").children(".kintai_type").val();
-    console.log(year,month,day,start_time,end_time,regist_type);
-    if( start_time && end_time && regist_type){
+    console.log(year,month,day,start_time,stop_time,regist_type);
+    if( start_time && stop_time && regist_type){
       $.ajax({
         type: "POST",
         url: "/kintai/" + year + "/" + ('0' + month).slice(-2) + "/" + ('0' + day).slice(-2) + "/",
-        data: "startTime=" + start_time + "&stopTime=" + end_time + "&type=" + regist_type 
+        data: "startTime=" + start_time + "&stopTime=" + stop_time + "&type=" + regist_type 
       });
     }
+  };
+  $("input").change(function(e){
+    console.log(e,this);
   });
 });
