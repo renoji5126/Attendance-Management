8a6af7a4b9adc93476556c4fcd9d6b12df720be5
renoji5126 <renoji5126@gmail.com>
2015-08-12 20:28:22 +0900

    fix

--- routes/users.js
+++ routes/users.js
@@ -27,7 +27,7 @@ router.get('/:id', function(req, res) {
     }
   },function(err, results){
     if(err) console.log(err.message);
-    optisons.db = results;
+    optisions.db = results;
     res.render('user', optisons);
   });
 });
