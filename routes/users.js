d50c2127058a7a9205ddee591ad613bb9b5690cd
renoji5126 <renoji5126@gmail.com>
2015-08-12 20:23:02 +0900

    fix

--- routes/users.js
+++ routes/users.js
@@ -6,32 +6,26 @@ var router = express.Router();
 var mongoose = module.parent.exports.mongoose;
 var userinfo = module.parent.exports.userInfoModel;
 var model = mongoose.models.syutokus;
-var ykmodel = mongoose.models.yuukyuusma;
-var dkmodel = mongoose.models.syukkinsma;
+var ykmodel = mongoose.models.yuukyuus;
+var dkmodel = mongoose.models.syukkins;
 
 /* GET users listing. */
 router.get('/:id', function(req, res) {
   var id = req.params.id;
   var query = { googleId : id };
   var optisions = {admin : req.session.passport.user.admin}
-  console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1");
   async.parallel({
     user : function(plcb){
-  console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa2");
       userinfo.find(query, function(err, docs){ plcb(err, docs[0]); });
     }, syutokus: function(plcb){
-  console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa3");
       model.find(query, function(err, docs){ plcb(err, docs); });
     }, yuukyuus: function(plcb){
-  console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa4");
       ykmodel.find(query, function(err, docs){ plcb(err, docs); });
     }, syukkins: function(plcb){
-  console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa5");
       dkmodel.find(query, function(err, docs){ plcb(err, docs); });
     //},function(plcb){
     }
   },function(err, results){
-  console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa6");
     console.log(err.message);
     optisons.db = results;
     res.render('user', optisons);
